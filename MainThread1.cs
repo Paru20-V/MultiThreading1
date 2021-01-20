59b9e8dbb2c24a589002de06bfbe6c2a75fa1fe4
Paru <parvathi95.v@gmail.com>
2021-01-20 14:50:41 -0800

    Add project files.

---
+++ MainThread1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreading
+{
+    class MainThread1
+    {
+        static void Main(string[] args)
+        {
+            Thread t = Thread.CurrentThread;
+             t.Name=" MainThread";
+            Console.WriteLine(t.Name);
+        }
+    }
+}
+
---
+++ ThreadExample1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MultiThreading
+{
+    class ThreadExample1
+    {
+        public void test1()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void test2()
+        {
+
+            for (int i = 21; i <= 30; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            ThreadExample1 e1 = new ThreadExample1();
+            Thread t1 = new Thread(e1.test1);
+            Thread t2 = new Thread(ThreadExample1.test2);
+            t1.Start();
+            t2.Start();
+        }
+    }
+}
